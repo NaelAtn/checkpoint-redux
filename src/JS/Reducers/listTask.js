77e378c3e1a8ff5e7e83e43128a63adc9fc84673
NaelAtn <nael.atouani2020@gmail.com>
2022-03-21 15:40:45 +0100

    checkpoint

---
+++ src/JS/Reducers/listTask.js
@@ -0,0 +1,36 @@
+import { ADD_TASK, DELETE_TASK, DONE_TASK, EDIT_TASK } from "../ActionTypes/listTask";
+
+//initialeState
+const intialState = {
+    listTasks : [
+        {id: Math.random(),text:"task1",isDone:false},
+        {id: Math.random(),text:"task2",isDone:false}
+    ]
+
+}
+
+//pure  function
+
+const listReducer = (state=intialState,{type,payload}) =>{
+    switch(type){
+        case ADD_TASK :
+            return {...state,listTasks: [...state.listTasks,payload]};
+        
+        case DELETE_TASK :
+            return {...state,listTasks: state.listTasks.filter(el => el.id !== payload)}
+
+        case DONE_TASK :
+            return {...state, listTasks : state.listTasks.map((el) =>(el.id === payload ? {...el, isDone:  !el.isDone} : el) )}    
+        
+        case EDIT_TASK :
+            return {...state, listTasks: state.listTasks.map((el) => (el.id === payload.id ? {...el, text:payload.newTask} : el))}
+
+
+
+        default:
+            return state;
+    }
+
+}
+
+export default listReducer
